b30cbf2c029df17e8a846205163b7e8d3351011b
jflevis <89398420+jflevis@users.noreply.github.com>
2021-11-30 14:38:00 -0500

    Add files via upload

---
+++ Memoire/Memoire/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Memoire
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMemoire = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMemoire
+            // 
+            this.btnMemoire.Location = new System.Drawing.Point(273, 102);
+            this.btnMemoire.Name = "btnMemoire";
+            this.btnMemoire.Size = new System.Drawing.Size(252, 172);
+            this.btnMemoire.TabIndex = 0;
+            this.btnMemoire.Text = "exercices données en mémoire";
+            this.btnMemoire.UseVisualStyleBackColor = true;
+            this.btnMemoire.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMemoire);
+            this.Name = "Form1";
+            this.Text = "frmExerciceMem";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMemoire;
+    }
+}
+
---
+++ Memoire/Memoire/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// lab6 mémoire programmation système JF Rancourt 2021-11-30
+namespace Memoire
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte octet;
+            char lettre;
+            int resultat;
+
+            string chaine;
+            
+            
+          byte[] tOctets = { 123, 124, 125, 126, 127, 128, 129, 130 };
+            char[] tCarac = { 'L', 'u', 't', 'i', 'n' };
+
+            octet = 32;
+            lettre = 'Z';
+            resultat = -1;
+
+            tOctets[4] = 1;
+            chaine = "Joyeux Noël";
+            lettre = chaine[3];
+            tCarac = chaine.ToCharArray();
+            tCarac[0] = 'j';
+
+            resultat = Fonction1(lettre, chaine, ref tOctets[4]);
+
+        }
+        int Fonction1(char pCar, string pCaracs, ref byte lo_ol)
+        {
+            string chaine = "Comment ça va ?";
+            pCaracs = "allo";
+            byte b;
+            byte[] tOctets = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            b = 77;
+            pCar = (char)(chaine[7] + tOctets[2]); // vérifier dans une table ASCII
+            lo_ol = b;
+
+            return tOctets[1];
+        }
+
+    }
+}
